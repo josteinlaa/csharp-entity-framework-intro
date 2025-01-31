e2dc1a1f0772b0d7131e64cf685ea3b0261b4ac6
Jostein Lind Aanby <jostein.aanby@no.experis.com>
2025-01-31 13:23:40 +0100

    finish

--- exercise.webapi/Data/DataContext.cs
+++ exercise.webapi/Data/DataContext.cs
@@ -2,20 +2,25 @@
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Reflection.Emit;
+using Microsoft.Extensions.Configuration;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 namespace exercise.webapi.Data
 {
     public class DataContext : DbContext
     {
-
+        private readonly string _connectionString;
+        
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
-
+             var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            _connectionString = configuration.GetValue<string>("ConnectionStrings:DefaultConnectionString")!;
+            this.Database.EnsureCreated();
         }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseInMemoryDatabase("Library");
+            optionsBuilder.UseNpgsql(_connectionString);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- exercise.webapi/Program.cs
+++ exercise.webapi/Program.cs
@@ -2,6 +2,7 @@ using exercise.webapi.Data;
 using exercise.webapi.Endpoints;
 using exercise.webapi.Repository;
 using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,18 +10,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Library"));
+builder.Services.AddDbContext<DataContext>();
 builder.Services.AddScoped<IBookRepository, BookRepository>();
 builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
 
 
 var app = builder.Build();
-
-using (var dbContext = new DataContext(new DbContextOptions<DataContext>()))
-{
-    dbContext.Database.EnsureCreated();
-}
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
